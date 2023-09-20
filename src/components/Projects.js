e7e3c256b439f66548edbd54f0d97cc4ec71e83e
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-20 22:23:40 +0300

    design: #2 - création du composant projects et de la const project

---
+++ src/components/Projects.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+export const Projects = () => {
+  const project = [
+    {
+      preview: "/assets/img/booki-preview.png",
+      url: "https://katd3v.github.io/booki-starter-code/",
+      alt: "aperçu du site booki",
+      github: "https://katd3v.github.io/booki-starter-code/",
+    },
+  ];
+
+  return <div>Projects</div>;
+};

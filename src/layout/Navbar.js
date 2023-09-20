617105961b7914151ed490385a97deaf2e340e3b
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-20 15:41:57 +0300

    design: #2 - Création du Layout et du composant Navbar contenant le menu

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -1,5 +1,36 @@
 import React from "react";
 
 const Navbar = () => {
-  return <div>header</div>;
+  return (
+    <header className="flex flex-col md:flex-row items-center bg-primary py-4">
+      <a href="#home">
+        <img
+          className="w-32 md:w-64 rounded-full"
+          src="/logo.jpeg"
+          alt="logo des initiales KS"
+        />
+      </a>
+      <nav className="w-full mt-4">
+        <ul className="flex justify-evenly">
+          <li>
+            <a href="#home">Accueil</a>
+          </li>
+          <li>
+            <a href="#about">À Propos</a>
+          </li>
+          <li>
+            <a href="#projects">Projets</a>
+          </li>
+          {/* <li>
+            <a href="#skills">Compétences</a>
+          </li> */}
+          <li>
+            <a href="#contact">Contact</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
 };
+
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
+    colors: {
+      primary: "#DDD0C8",
+      secondary: "#301004",
+    },
+    fontFamily: {
+      sans: ["Montserrat", "sans-serif"],
+    },
     extend: {},
   },
   plugins: [],

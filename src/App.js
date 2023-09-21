e2a2cd443b8e9b1fd5988565e0850eae82517280
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-21 11:18:11 +0300

    design: #2 - développement de la structure du composant projects et cards

--- src/App.js
+++ src/App.js
@@ -4,12 +4,14 @@ import { Layout } from "./layout/Layout";
 import Hero from "./components/Hero";
 import About from "./components/About";
 import Contact from "./components/Contact";
+import { Projects } from "./components/Projects";
 
 function App() {
   return (
     <Layout>
       <Hero />
       <About />
+      <Projects />
       <Contact />
     </Layout>
   );

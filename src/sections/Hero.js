36d71ae714618c649815bd4aeab5ba6b3596d57d
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-24 13:43:11 +0200

    feat: #3 - ajout de l'animation dans le composant hero + image en background

--- src/sections/Hero.js
+++ src/sections/Hero.js
@@ -1,14 +1,37 @@
 import React from "react";
+import Typewriter from "typewriter-effect";
 
 const Hero = () => {
-  const typewrite = ["Katleen", "Développeur Web"];
+  const heroStyle = {
+    backgroundImage: `url("/assets/img/background-hero.jpg")`,
+    backgroundSize: "cover",
+  };
+
   return (
     <section
       id="home"
-      className="h-72 flex justify-center items-center bg-gradient-to-r from-primary to-bgBrown"
+      className="h-72 flex justify-center items-center"
+      style={heroStyle}
     >
-      <h1 className="text-center text-3xl">
-        Bonjour, <br /> Je suis <span>{typewrite[0]}</span>
+      <h1 className="text-center text-3xl text-secondary">
+        Bonjour, <br /> Je suis{" "}
+        <span>
+          <Typewriter
+            onInit={(typewriter) => {
+              typewriter
+                .typeString("Katleen")
+                .pauseFor(1000)
+                .deleteAll()
+                .typeString("Développeur web")
+                .pauseFor(1000)
+                .start();
+            }}
+            options={{
+              loop: true, //Répète l'animation en boucle
+              autoStart: true, //Démarre l'animation automatiquement
+            }}
+          />
+        </span>
       </h1>
     </section>
   );

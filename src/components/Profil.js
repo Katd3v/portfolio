2d6742836f1eeda05cd88bfb9ec4f009ab20069d
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-09-24 14:51:37 +0200

    style: #4 - modification du style de la photo de profil et du texte

--- src/components/Profil.js
+++ src/components/Profil.js
@@ -3,18 +3,22 @@ import React from "react";
 export const Profil = () => {
   return (
     <>
-      <div>
-        <img
-          src="/assets/img/profil.png"
-          alt="Photo de profil de Katleen SORHAINDO"
-        />
-      </div>
+      <img
+        className="rounded-full my-4"
+        src="/assets/img/profil.png"
+        alt="Photo de profil de Katleen SORHAINDO"
+      />
       <h2>À propos de moi</h2>
       <p className="py-4">
-        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fugiat iure
-        possimus qui illo commodi eligendi repellat itaque. Assumenda ipsa
-        nostrum quibusdam magnam, quidem vel consectetur quos dolores
-        consequuntur cumque inventore.
+        Après une décennie passée à travailler dans une administration privé,
+        j'ai acquis de nombreuses compétences essentielles en gestion, en
+        communication, en résolution de problèmes, et en formation d'équipes.
+      </p>
+      <p>
+        Ma décision de me tourner vers le développement web découle de ma
+        passion pour la technologie et la création. J'ai investi du temps et de
+        l'énergie pour acquérir des compétences techniques en développement
+        front-end et back-end, ainsi qu'en conception web.
       </p>
     </>
   );

909eeb698933ef4d3329e770417e599d17d39a8c
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-10-01 17:25:59 +0200

    refactor: #9 - ajout composant reseaux avec linkedin, contact et github)

--- src/layout/Footer.js
+++ src/layout/Footer.js
@@ -1,39 +1,10 @@
 import React from "react";
+import Reseau from "../components/Reseaux";
 
 export const Footer = () => {
   return (
     <footer className="bg-secondary">
-      <div>
-        <ul className="flex">
-          <li>
-            <a href="mailto:katleen.sorhaindo@gmail.com">
-              <img
-                className="h-12"
-                src="/assets/img/email-logo.png"
-                alt="logo email pour me contacter par mail"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="https://github.com/Katd3v">
-              <img
-                className="h-12"
-                src="/assets/img/github-logo.png"
-                alt="logo github pour accéder à mon github"
-              />
-            </a>
-          </li>
-          <li>
-            <a href="/">
-              <img
-                className="h-12"
-                src="/assets/img/linkedin-logo.png"
-                alt="logo linkedin pour voir mon profil linkedin"
-              />
-            </a>
-          </li>
-        </ul>
-      </div>
+      <Reseau />
       <p>© 2023 Katleen SORHAINDO, Tous droits réservés.</p>
     </footer>
   );

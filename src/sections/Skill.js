87cec79224cbdcdb05ebad84bd8328f2eaa9eb2f
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-10-05 23:34:08 +0200

    feat: ajout description a la section skill

--- src/sections/Skill.js
+++ src/sections/Skill.js
@@ -2,6 +2,16 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Skills from "../components/Skills";
 
+const descriptionSkill = {
+  language:
+    "J'ai développé des bases solides dans les langages du web, notamment HTML5 pour la création de structures web sémantiques, CSS pour la conception de mises en page élégantes et réactives, ainsi que JavaScript pour l'ajout de fonctionnalités interactives et dynamiques à mes projets y compris la manipulation du DOM, la gestion des événements, et l'utilisation de la POO (programmation orientée objet).",
+
+  framework:
+    "J'ai développé une expertise dans un ensemble de frameworks et de technologies essentielles, notamment React pour le développement d'interfaces utilisateur réactives et modernes, Node.js pour la création de serveurs robustes, Express pour la gestion des routes et des requêtes HTTP, ainsi que Sass et Tailwind CSS pour la conception de styles modulaires et de designs élégants.",
+
+  tool: "Je suis familiarisée avec une gamme d'outils essentiels pour le développement web, notamment Git et GitHub pour la gestion de version et la collaboration, MongoDB pour la gestion de bases de données NoSQL, Trello pour la planification et la gestion de projet, ainsi que Figma pour la conception et la collaboration en design. Ces outils me permettent de travailler de manière efficace, d'assurer un suivi précis des projets et de collaborer efficacement au sein d'une équipe de développement",
+};
+
 export const Skill = () => {
   const [skills, setSkills] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -21,15 +31,33 @@ export const Skill = () => {
         setLoading(false);
       });
   }, []);
+
+  const getSkillDescription = (category) => {
+    return descriptionSkill[category];
+  };
+
   return (
-    <section id="skills" className="my-4 bg-primary">
-      <h3 className="text-left text-secondary ml-4">Mes compétences</h3>
+    <section id="skills" className=" bg-secondary py-10">
+      <h2 className=" text-white text-center ml-4">Mes compétences</h2>
+      <p>
+        Durant ma formation intensive de 6 mois, je suis monté en compétence
+        dans divers outils et technologies, me permettant de concevoir des sites
+        web optimisés et fonctionnels. Ces compétences et stacks technologiques
+        constituent une base solide pour un développeur web junior. Ils couvrent
+        à la fois les aspects de conception et de développement, ainsi que la
+        gestion de projet, ce qui me permettra de travailler efficacement sur
+        des projets web et de continuer à m'améliorer dans le domaine du
+        développement web.
+      </p>
       {loading ? (
         <div>Chargement en cours...</div>
       ) : (
-        <div className="px-4 my-4 flex flex-wrap items-start justify-around gap-x-8 gap-y-10 sm:gap-x-10 lg:mx-0">
+        <div>
           {skills.map((skill, index) => (
-            <Skills key={index} skills={skill} />
+            <div key={index}>
+              <Skills skills={skill} />
+              <p>{getSkillDescription(skill.category)}</p>
+            </div>
           ))}
         </div>
       )}

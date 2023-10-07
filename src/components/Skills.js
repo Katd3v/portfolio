65d6fad968f03f2d6ab7b7396c8cfba26d03a6c4
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-10-07 23:03:04 +0200

    style: #23 - modification affichage carte des skills en responsive

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -2,27 +2,23 @@ import React from "react";
 
 const Skills = ({ skills }) => {
   return (
-    <>
-      {skills.map((skill) => (
-        <div className="my-8 flex justify-center">
-          <div className="max-w-[300px] sm:max-w-[352px] md:h-[440px] bg-primary rounded-xl overflow-hidden shadow-md">
-            <div className="w-full max-h-[235px]">
-              <img
-                className="object-cover w-[352px] h-[235px]"
-                src={skill.imageUrl}
-                alt={skill.title}
-              />
-            </div>
-            <div className="p-6">
-              <h3 className="font-bold text-2xl text-secondary">
-                {skill.title}
-              </h3>
-              <p className="">{skill.description}</p>
-            </div>
-          </div>
+    <div className="my-12 flex justify-center flex-wrap">
+      {/* {skills.map((skill, index) => ( */}
+      <div className="max-w-[300px] md:h-[440px] xl:max-w-[352px] bg-primary rounded-xl overflow-hidden shadow-md">
+        <div className="w-full max-h-[235px]">
+          <img
+            className="object-cover w-[352px] h-[235px]"
+            src={skills.imageUrl}
+            alt={skills.title}
+          />
         </div>
-      ))}
-    </>
+        <div className="p-6">
+          <h3 className="font-bold text-2xl text-secondary">{skills.title}</h3>
+          <p className="">{skills.description}</p>
+        </div>
+      </div>
+      {/* ))} */}
+    </div>
   );
 };
 
--- src/sections/Skill.js
+++ src/sections/Skill.js
@@ -2,17 +2,6 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import Skills from "../components/Skills";
 
-// const descriptionSkill = {
-//   langages:
-//     "J'ai développé des bases solides dans les langages du web, notamment HTML5 pour la création de structures web sémantiques, CSS pour la conception de mises en page élégantes et réactives, ainsi que JavaScript pour l'ajout de fonctionnalités interactives et dynamiques à mes projets y compris la manipulation du DOM, la gestion des événements, et l'utilisation de la POO (programmation orientée objet).",
-
-//   frameworks:
-//     "J'ai développé une expertise dans un ensemble de frameworks et de technologies essentielles, notamment React pour le développement d'interfaces utilisateur réactives et modernes, Node.js pour la création de serveurs robustes, Express pour la gestion des routes et des requêtes HTTP, ainsi que Sass et Tailwind CSS pour la conception de styles modulaires et de designs élégants.",
-
-//   outils:
-//     "Je suis familiarisée avec une gamme d'outils essentiels pour le développement web, notamment Git et GitHub pour la gestion de version et la collaboration, MongoDB pour la gestion de bases de données NoSQL, Trello pour la planification et la gestion de projet, ainsi que Figma pour la conception et la collaboration en design. Ces outils me permettent de travailler de manière efficace, d'assurer un suivi précis des projets et de collaborer efficacement au sein d'une équipe de développement",
-// };
-
 export const Skill = () => {
   const [skills, setSkills] = useState([]);
   const [loading, setLoading] = useState(true);
@@ -33,33 +22,30 @@ export const Skill = () => {
       });
   }, []);
 
-  // const getSkillDescription = (category) => {
-  //   return (
-  //     descriptionSkill[category.toLowerCase()] || "Description non disponible"
-  //   );
-  // };
-
   return (
     <section id="skills" className=" bg-secondary py-10 px-4">
       <h2 className=" text-white text-center mb-12">Mes compétences</h2>
-      <div className="px-16">
-        <p className="text-white">
+      <div className="px-8 md:px-16">
+        <p className="text-white mb-6">
           Dans le cadre de ma reconversion professionnelle, j'ai consacré du
           temps à me former et à monter en compétence dans un éventail varié
           d'outils et de technologies essentiels au développement web.
         </p>
-        <p className="text-white my-2">
+        <p className="text-white my-4">
           Cette formation englobe des aspects de conception, de développement et
           de gestion de projet, ce qui me permet de concevoir des sites web
-          optimisés et fonctionnels. Ces compétences me préparent à travailler
-          efficacement sur une variété de projets web tout en continuant à
-          progresser dans le domaine du développement web.
+          optimisés et fonctionnels.
+        </p>
+        <p className="text-white my-6">
+          Ces compétences me préparent à travailler efficacement sur une variété
+          de projets web tout en continuant à progresser dans le domaine du
+          développement web.
         </p>
       </div>
       {loading ? (
         <div>Chargement en cours...</div>
       ) : (
-        <div>
+        <div className="md:flex md:flex-wrap md:justify-evenly xl:px-8">
           {skills.map((skill, index) => (
             <Skills key={index} skills={skill} />
           ))}

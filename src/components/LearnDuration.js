105b167fd2dd1bd6b1e1f4cfe8399d9f5461d5dd
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-10-08 01:11:13 +0200

    feat: #23 - ajout fonctionnalité pour calculer le temps d'apprentissage

---
+++ src/components/LearnDuration.js
@@ -0,0 +1,37 @@
+import React from "react";
+import moment from "moment";
+
+const LearnDuration = ({ startDate }) => {
+  // utilise moment pour la gestion des dates
+  const currentDate = moment();
+  const startDateTime = moment(startDate);
+
+  // calcul de la différence entre les dates
+  const duration = moment.duration(currentDate.diff(startDateTime));
+
+  // récupère en année, mois et jours
+  const years = duration.years();
+  const months = duration.months();
+  const days = duration.days();
+
+  return (
+    <div>
+      <p>
+        Temps de pratique :{" "}
+        {years > 0 && years < 2 ? (
+          <span> {years} an</span>
+        ) : (
+          years > 0 && <span> {years} ans</span>
+        )}
+        {months > 0 && <span> {months} mois</span>}
+        {days > 0 && days < 2 ? (
+          <span> {days} jour</span>
+        ) : (
+          <span> {days} jours</span>
+        )}
+      </p>
+    </div>
+  );
+};
+
+export default LearnDuration;

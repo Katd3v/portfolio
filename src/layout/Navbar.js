066a829c32aa2a3eb9fb48fff03464f203e92ff5
Katleen SORHAINDO <katleen.clerima@hotmail.fr>
2023-10-09 19:56:37 +0200

    refactor: ajout onClick sur navbar

--- src/layout/Navbar.js
+++ src/layout/Navbar.js
@@ -83,6 +83,7 @@ export default function Navbar() {
                     key={item.name}
                     href={item.href}
                     className="-mx-3 block rounded-lg px-3 py-2 text-base font-semibold leading-7 text-secondary hover:bg-gray-50"
+                    onClick={() => setMobileMenuOpen(false)}
                   >
                     {item.name}
                   </a>
